442fb59323a0062428763159c1c588c739c06dd0
Daniyal Saud <danialsaud7@gmail.com>
2025-12-20 11:37:21 +0500

    Add @vercel/react-router and apply Vercel preset
    Added @vercel/react-router to dependencies and updated react-router.config.ts to use the Vercel preset for Vite integration. This enables improved compatibility and deployment with Vercel.

--- react-router.config.ts
+++ react-router.config.ts
@@ -1,5 +1,5 @@
 import type { Config } from "@react-router/dev/config";
-
+import { vercelPreset} from "@vercel/react-router/vite"
 export default {
   // Config options...
   // Server-side render by default, to enable SPA mode set this to `false`
@@ -7,5 +7,6 @@ export default {
   // Setting unstable_middleware to true will run the route loaders
   future: {
     v8_middleware: true
-  }
+  },
+  presets: [vercelPreset()]
 } satisfies Config;
