d237bbaa50a04a3cefeb9c62f2b37963255de7ec
Daniyal Saud <danialsaud7@gmail.com>
2025-12-21 17:03:29 +0500

    Update react-router.config.ts

--- react-router.config.ts
+++ react-router.config.ts
@@ -1,11 +1,12 @@
 import type { Config } from "@react-router/dev/config";
-
+import { vercelPreset } from "@vercel/react-router/vite";
 export default {
   // Config options...
   // Server-side render by default, to enable SPA mode set this to `false`
   ssr: true,
   // Setting unstable_middleware to true will run the route loaders
   future: {
-    v8_middleware: true
+    v8_middleware: true,
   },
+  presets: [vercelPreset()],
 } satisfies Config;
