f492a72f3ea9c9099a2786cb4b3c4306e7236c57
Daniyal Saud <danialsaud7@gmail.com>
2025-12-22 12:28:05 +0500

    Update auth-client.ts

--- app/lib/auth-client.ts
+++ app/lib/auth-client.ts
@@ -3,6 +3,6 @@ import type { auth } from "@/lib/auth.server";
 import { inferAdditionalFields } from "better-auth/client/plugins";
 
 export const authClient = createAuthClient({
-    baseURL: import.meta.env.VITE_BASE_URL || (typeof window !== 'undefined' ? window.location.origin : 'http://localhost:3000'),
+    baseURL: process.env.VITE_BASE_URL as string,
     plugins: [inferAdditionalFields<typeof auth>()],
 });
